caf6d5a5d0fa6e8681735f24520d54158bcec1dd
gaianour <gaia.marsegaglia@liceobanfi.eu>
2023-06-03 09:54:56 +0200

    commit inserimento

--- pagine/registrazione.php
+++ pagine/registrazione.php
@@ -90,9 +90,7 @@ require("../database/dati_db.php");
                         die("<p>Connessione al server non riuscita: ".$conn->connect_error."</p>");
                     }
                     
-                    $myquery = "SELECT username 
-						    FROM account 
-						    WHERE username='" . $_POST["username"] . "'";
+                    
                     
 
                     $ris = $conn->query($myquery) or die("<p>Query fallita!</p>");
